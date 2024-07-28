32ea3e294f4f8c3f282b9fec2db62854a2e2e2e1
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-07-28 00:12:57 -0300

    28/07/2024 atualização da Section 4 e conclusão dela.
    Atualização da Section 4 e conclusão dela. Preparei a sessão para ter váriaveis com useState para mudar o contador dinâmicamente depois. Como eu não tenho as manhas ainda, vou ver DEPOIS se consigo implementar essa funcionalidade com apoio dos meus materiais de estudo. :)

--- src/Componentes/Section4/index.js
+++ src/Componentes/Section4/index.js
@@ -1,31 +1,37 @@
+import React, { useState } from 'react';
 import './Section4.css';
-import retanguloContador from '../img/Rectangle 4.png'
-import barraDaCaixaDeFotos from '../img/Rectangle 6.png'
-import circuloVermelho from '../img/Ellipse 5.png'
-import circuloAmarelo from '../img/Ellipse 4.png'
-import circuloVerde from '../img/Ellipse 3.png'
-import Foto7 from '../img/Foto 7.png'
-import Foto8 from '../img/Foto 8.png'
-import Foto9 from '../img/Foto 9.png'
-import Foto10 from '../img/Foto 10.png'
-import Foto11 from '../img/Foto 11.png'
+import retanguloContador from '../img/Rectangle 4.png';
+import barraDaCaixaDeFotos from '../img/Rectangle 6.png';
+import circuloVermelho from '../img/Ellipse 5.png';
+import circuloAmarelo from '../img/Ellipse 4.png';
+import circuloVerde from '../img/Ellipse 3.png';
+import Foto7 from '../img/Foto 7.png';
+import Foto8 from '../img/Foto 8.png';
+import Foto9 from '../img/Foto 9.png';
+import Foto10 from '../img/Foto 10.png';
+import Foto11 from '../img/Foto 11.png';
 
 const Section4 = (props) => {
+    // Estado para os contadores (Contador de projetos da div "contador")
+
+    /* eslint-disable */ /* <-- Comando para desligar os warnings no terminal das váriaveis que AINDA não estão sendo usadas. Esse comando é feito como um comentario mesmo, do jeito que está ali. NÃO APAGAR, pelo menos enquanto NÃO ESTEJA USANDO as váriaveis abaixo. :)*/
+
+    const [quantidadeProjetos, setQuantidadeProjetos] = useState(50);
+    const [projetosAndamento, setProjetosAndamento] = useState(38);
+    const [projetosCompletos, setProjetosCompletos] = useState(98);
+
     return (
         <div className='container'>
             <section className='section4'>
 
                 <div className='quadroDeFoto'>
-                    <div class="barraContainer">
-                        <img src={barraDaCaixaDeFotos} alt='Barra da caixa de fotos'/>
-                        <img src={circuloVermelho} alt='Circulo vermelho da barra da caixa de fotos'className='circuloVermelho'/>
-
-                        <img src={circuloAmarelo} alt='Circulo amarelo da barra da caixa de fotos'className='circuloAmarelo'/>
-
-                        <img src={circuloVerde} alt='Circulo verde da barra da caixa de fotos'className='circuloVerde'/>
+                    <div className="barraContainer">
+                        <img src={barraDaCaixaDeFotos} alt='Barra da caixa de fotos' className='barraDaCaixaDeFotos'/>
+                        <img src={circuloVermelho} alt='Circulo vermelho da barra da caixa de fotos' className='circuloVermelho'/>
+                        <img src={circuloAmarelo} alt='Circulo amarelo da barra da caixa de fotos' className='circuloAmarelo'/>
+                        <img src={circuloVerde} alt='Circulo verde da barra da caixa de fotos' className='circuloVerde'/>
                     </div>
 
-                    {/*FOTOS DO QUADRO ⬇️*/}
                     <div className='fotosDaCaixa'>
                         <img src={Foto7} alt='Foto7' className='Foto7'/>
                         <img src={Foto8} alt='Foto8' className='Foto8'/>
@@ -36,21 +42,36 @@ const Section4 = (props) => {
                 </div>
 
                 <div className='textoSection4'>
-                    <h1>Experimentando e <br></br>
-                        personalizando.</h1>
+                    <h1>Experimentando e <br /> personalizando.</h1>
                     <p>
-                        Uma sequencia de teste dahora aqui para ver<br></br>
-                        como se comporta essa parte, podia usar lorem,<br></br>
+                        Uma sequencia de teste dahora aqui para ver<br />
+                        como se comporta essa parte, podia usar lorem,<br />
                         mas vou escrever lorota mesmo :)
                     </p>
                 </div>
 
                 <div className='contador'>
                     <img src={retanguloContador} alt='Retangulo branco onde fica os contadores'/>
+
+                    <div className='contadores'>
+                        <div className='contador-item'>
+                            <div className='count'>{quantidadeProjetos}+</div>
+                            <div className='label'>Quantidade de projetos</div>
+                        </div>
+                        <div className='contador-item'>
+                            <div className='count'>{projetosAndamento}+</div>
+                            <div className='label'>Projetos em andamento</div>
+                        </div>
+                        <div className='contador-item'>
+                            <div className='count'>{projetosCompletos}+</div>
+                            <div className='label'>Projetos Completos</div>
+                        </div>
+                    </div>
                 </div>
+
             </section>
         </div>
-    )
+    );
 }
 
 export default Section4;
