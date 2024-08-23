fe3836a2e637cbc17ed39a3ac268da42bb8d344e
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-08-23 15:03:16 -0300

    Criação do Componente Carousel para a galeria
    Criação do Componente Carousel para a galeria
    Criação do componente e inserção do mesmo na Section2 para começar a trabalhar com a galeria de desenhos.
    
    Até então, inseridos apenas 2 Galerias. Aumentar a quantidade de itens da galeria e finalizar as demais.
    
    23/08/2024

--- src/Componentes/Arts/Arts.js
+++ src/Componentes/Arts/Arts.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ArtsSection1 from './Section1';
 import ArtsSection2 from './Section2';
-import CarouselImagesDiego from './Carousel';
-
 
 
 const Arts = (props) => {
   return (
     <div> 
-        <ArtsSection1/>
-        <ArtsSection2/>
-        
+        <ArtsSection1 />
+        <ArtsSection2 />
     </div>
-  );
+  )
 };
 
 export default Arts;
--- src/Componentes/Arts/Section2/index.js
+++ src/Componentes/Arts/Section2/index.js
@@ -1,16 +1,50 @@
-import './ArtsSection2.css'
+import React from 'react';
+import CarouselBS4 from '../Carousel/index'
+import Carousel1 from '../../Images/carousel1.jpg';
+import Carousel2 from '../../Images/carousel2.jpg';
+import Carousel3 from '../../Images/carousel3.jpg';
+
+import Carousel10 from '../../Images/carousel10.jpg';
+import Carousel11 from '../../Images/carousel11.jpg';
+import Carousel12 from '../../Images/carousel12.jpg';
 
 const ArtsSection2 = () => {
+  const animeImages = [
+    { src: Carousel1, alt: 'First slide', label: 'Anime Slide 1', caption: 'First anime image description' },
+    { src: Carousel2, alt: 'Second slide', label: 'Anime Slide 2', caption: 'Second anime image description' },
+    { src: Carousel3, alt: 'Third slide', label: 'Anime Slide 3', caption: 'Third anime image description' }
+  ];
+
+  const celebridadesImages = [
+    { src: Carousel10, alt: 'First slide', label: 'Celebridade Slide 1', caption: 'First Celebridade image description' },
+    { src: Carousel11, alt: 'Second slide', label: 'Celebridade Slide 2', caption: 'Second Celebridade image description' },
+    { src: Carousel12, alt: 'Third slide', label: 'Celebridade Slide 3', caption: 'Third Celebridade image description' }
+  ];
+
   return (
     <div>
       <section>
-        <div className='textoArtsSection2 container'>
-          <h1>Desenho de Animais</h1>
-          <p>Descrição pequena dessa parte</p>
-        </div>
+      {/* Reutilizando o componente para a galeria de animes */}
+        <article>
+          {/*Sessão de animes*/}
+          <CarouselBS4
+            title="Desenhos de Anime"
+            description="Galeria de desenhos inspirados em animes"
+            images={animeImages}
+          />
+        </article>
+
+        <article>
+          <CarouselBS4 
+            title="Desenhos de celebridades"
+            description="Galeria de desenhos inspirados em celebridades"
+            images={celebridadesImages}
+          />
+        </article>
+
       </section> 
     </div>
-  )
+  );
 }
 
 export default ArtsSection2;
